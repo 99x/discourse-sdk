f034967ef0ec40e10cdd9a2fcb7f0d49004d4897
niroshan <niroshanrd.13@cse.mrt.ac.lk>
2016-10-16 19:41:33 +0530

    test commit

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 //exports = module.exports = require('./lib/discourse');
-var dis=require('./lib/discourse');
-var my=dis('http://forum.99xtechnology.net','51521cf327d4033c815f761e9c36a61b2a9b6b262d6e826ef8dc4e6c93edcd6e','niroshanr');
+var dis=require('./lib/discourse');;
