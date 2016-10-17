dc03e0b11c5e5900094d7b2e4c4d3d5ac69d66bf
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-10-17 16:59:11 +0530

    commit for few changes

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-//exports = module.exports = require('./lib/discourse');
-var dis=require('./lib/discourse');
-var my=dis('http://forum.99xtechnology.net','51521cf327d4033c815f761e9c36a61b2a9b6b262d6e826ef8dc4e6c93edcd6e','niroshanr');
+exports = module.exports = require('./lib/discourse');
