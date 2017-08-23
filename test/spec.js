bcaf5a5c179a149af374b2bda3ff7eefe9e9a3b5
De Silva, Dinuka <l.dinukadesilva@gmail.com>
2017-08-23 15:41:21 +0530

    gh-14: Initial test setup with mocha and nyc
    This closes #14

---
+++ test/spec.js
@@ -0,0 +1,9 @@
+var discourceSdk = require("../index");
+
+describe("discourse-sdk", function () {
+  //TODO add test cases
+
+  it("should be injectable as a common js module", function () {
+    discourceSdk;
+  });
+});
